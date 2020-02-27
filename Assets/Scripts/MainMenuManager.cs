ae08c9c0cc93e7c01fe39165bbbfd978ef8a03fe
deniztraka <deniz.traka@gmail.com>
2020-02-28 00:23:49 +0300

    main menu

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public void StartGame(){
+        // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
+        SceneManager.LoadScene("GameScene", LoadSceneMode.Single);
+        //Debug.Log("hede");
+    }
+
+    public void Quit(){
+        Application.Quit();
+    }
+}
