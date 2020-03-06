dad615daa19f1ef4529e654dd38a27736a8ba5f1
Deniz Traka <d.traka@tr.emakina.com>
2020-03-06 23:04:17 +0300

    running animal spawner

---
+++ Assets/Scripts/RunnerAnimal.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunnerAnimal : MonoBehaviour
+{
+    private CharacterController2D controller;
+    public float MovementSpeed = 20;
+    public float HorizontalMove = 0;
+
+    void Start()
+    {
+        controller = gameObject.GetComponent<CharacterController2D>();
+    }
+    void FixedUpdate()
+    {
+        // Move our character        
+        controller.Move(HorizontalMove * MovementSpeed * Time.fixedDeltaTime, false, false);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            //Debug.Log(other.gameObject.name + " : " + gameObject.name + " : " + Time.time);
+            GameObject.Destroy(other.gameObject);
+            
+        }
+
+    }
+}
---
+++ Assets/Scripts/RunningAnimalSpawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunningAnimalSpawner : MonoBehaviour
+{
+    public List<GameObject> AnimalList;
+    private GameObject[] animalListArray;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animalListArray = AnimalList.ToArray();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            //Debug.Log(other.gameObject.name + " : " + gameObject.name + " : " + Time.time);
+            GameObject.Destroy(this.gameObject);
+            Spawn();
+        }
+
+    }
+
+    void Spawn()
+    {
+        var index = Random.Range(0, AnimalList.Count);
+
+        var instantiatedObj = Instantiate(animalListArray[index], new Vector3(this.gameObject.transform.position.x - 12, 5, 0), animalListArray[index].transform.rotation);
+        var characterController2D = instantiatedObj.GetComponent<CharacterController2D>();
+        //characterController2D.
+    }
+}
